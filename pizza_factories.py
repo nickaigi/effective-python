0b380fd2dc37f8090fba681a2f91ebdaa6be26f8
Nickson Kaigi <nickaigi@gmail.com>
2019-01-23 06:44:52 +0300

    pizza factories

---
+++ pizza_factories.py
@@ -0,0 +1,41 @@
+import math
+
+
+class Pizza(object):
+    """ Demonstrates how to effectively use classmethod
+    and the power of staticmethod
+    """
+    def __init__(self, radius, ingredients):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        # TODO what does __repr__ do
+        # TODO what are f strings
+        return (
+            f'Pizza({self.radius!r}, '
+            f'{self.ingredients!r})'
+        )
+
+    @classmethod
+    def margherita(cls):
+        return cls(5, ['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(8, ['mozzarella', 'tomatoes', 'ham'])
+
+    def area(self):
+        return self.circle_area(self.radius)
+
+    @staticmethod
+    def circle_area(r):
+        return r ** 2 * math.pi
+
+
+if __name__ == '__main__':
+    margherita = Pizza.margherita()
+    prosciutto = Pizza.prosciutto()
+
+    print(margherita.area())
+    print(Pizza.circle_area(4))
