18600a08d9d11f3cc1457ef74d6aa62794e72846
Nickson Kaigi <nickaigi@gmail.com>
2019-04-01 17:12:36 +0300

    found the issue

--- item_34_registerclsexistances_with_metacls.py
+++ item_34_registerclsexistances_with_metacls.py
@@ -120,8 +120,31 @@ class Point3D(BetterSerializable):
         self.z = z
 
 
-def main():
+class Meta(type):
+    def __new__(meta, name, bases, class_dict):
+        cls = type.__new__(meta, name, bases, class_dict)
+        register_class(cls)
+        return cls
+
+
+class RegisteredSerializable(BetterSerializable, metaclass=Meta):
+    """pass
+    """
     pass
+
+
+class Vector3D(RegisteredSerializable):
+    def __init__(self, x, y, z):
+        super().__init__(x, y, z)
+        self.x, self.y, self.z = x, y, z
+
+
+def main():
+    v3 = Vector3D(10, -7, 3)
+    print('Before:     ', v3)
+    data = v3. serialize()
+    print('Serialized:', data)
+    print('After:     ', deserialize(data))
 
 
 if __name__ == '__main__':
