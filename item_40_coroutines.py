a0ec09344ab0661f407a442016653edb3b312862
Nickson Kaigi <nickaigi@gmail.com>
2019-04-11 23:04:07 +0300

    added example three

--- item_40_coroutines.py
+++ item_40_coroutines.py
@@ -66,9 +66,42 @@ def count_neighbors(y, x):
     w_ = yield Query(y + 0, x - 1)  # West
     nw = yield Query(y + 1, x - 1)  # NorthWest
 
+    neighbor_states = [n_, ne, e_, se, s_, sw, w_, nw]
+    count = 0
+    for state in neighbor_states:
+        if state == ALIVE:
+            count += 1
+    return count
+
+
+def example_three():
+    """
+    >>> 
+    First yield:  Query(y=11, x=5)
+    Second yield:  Query(y=11, x=6)
+    ...
+    Count:  2
+    """
+    it = count_neighbors(10, 5)
+    q1 = next(it)                # Get the first query
+    print('First yield: ', q1)
+    q2 = it.send(ALIVE)          # Send q1 state, get q2
+    print('Second yield: ', q2)
+    q3 = it.send(ALIVE)          # Send q2 state, get q3
+    print('...')
+    q4 = it.send(EMPTY)
+    q5 = it.send(EMPTY)
+    q6 = it.send(EMPTY)
+    q7 = it.send(EMPTY)
+    q8 = it.send(EMPTY)
+    try:
+        it.send(EMPTY)         # Send q8 state, retrieve count
+    except StopIteration as e:
+        print('Count: ', e.value)  # value form return statement
+
 
 def main():
-    example_two()
+    example_three()
 
 
 if __name__ == '__main__':
