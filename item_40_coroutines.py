d54725746ffad683fc8392ff8c234c1acba87dc5
Nickson Kaigi <nickaigi@gmail.com>
2019-04-13 18:30:58 +0300

    done for the week

--- item_40_coroutines.py
+++ item_40_coroutines.py
@@ -5,6 +5,7 @@ ALIVE = '*'
 EMPTY = '-'
 Query = namedtuple('Query', ('y', 'x'))
 Transition = namedtuple('Transition', ('y', 'x', 'state'))
+TICK = object()
 
 
 def my_coroutine():
@@ -141,6 +142,110 @@ def example_four():
     q8 = it.send(EMPTY)
     t1 = it.send(EMPTY)    # Send for q8, get same decision
     print('Outcome: ', t1)
+
+
+def simulate(height, width):
+    while True:
+        for y in range(height):
+            for x in range(width):
+                yield from step_cell(y, x)
+        yield TICK
+
+
+class Grid(object):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rows = []
+        for _ in range(self.height):
+            self.rows.append([EMPTY] * self.width)
+
+    def __str__(self):
+        output = ''
+        for row in self.rows:
+            for cell in row:
+                output += cell
+            output += '\n'
+        return output
+
+    def query(self, y, x):
+        return self.rows[y % self.height][x % self.width]
+
+    def assign(self, y, x):
+        self.rows[y % self.height][x % self.width] = state
+
+
+def live_a_generation(grid, sim):
+    progeny = Grid(grid.height, grid.width)
+    item = next(sim)
+    while item is not TICK:
+        if isinstance(item, Query):
+            state = grid.query(item.y, item.x)
+            item = sim.send(state)
+        else:  # must be a transition
+            progeny.assign(item.y, item.x, item.state)
+            item = next(sim)
+
+
+def example_five():
+    grid = Grid(5, 9)
+    grid.assign(0, 3, ALIVE)
+    grid.assign(1, 4, ALIVE)
+    grid.assign(2, 2, ALIVE)
+    grid.assign(2, 3, ALIVE)
+    grid.assign(2, 4, ALIVE)
+    print(grid)
+
+
+class ColumnPrinter(object):
+    def __init__(self):
+        self.columns = []
+
+    def append(self, data):
+        self.columns.append(data)
+
+    def __str__(self):
+        row_count = 1
+        for data in self.columns:
+            row_count = max(row_count, len(data.splitlines()) + 1)
+        rows = [''] * row_count
+        for j in range(row_count):
+            for i, data in enumerate(self.columns):
+                line = data.splitlines()[max(0, j - 1)]
+                if j == 0:
+                    padding = ' ' * (len(line) // 2)
+                    rows[j] += padding + str(i) + padding
+                else:
+                    rows[j] += line
+                if (i + 1) < len(self.columns):
+                    rows[j] += ' | '
+        return '\n'.join(rows)
+
+columns = ColumnPrinter()
+sim = simulate(grid.height, grid.width)
+for i in range(5):
+    columns.append(str(grid))
+    grid = live_a_generation(grid, sim)
+
+
+print(columns)
+
+
+grid = Grid(5,5)
+grid.assign(1, 1, ALIVE)
+grid.assign(2, 2, ALIVE)
+grid.assign(2, 3, ALIVE)
+grid.assign(3, 3, ALIVE)
+
+
+columns = ColumnPrinter()
+sim = simulate(grid.height, grid.width)
+for i in range(5):
+    columns.append(str(grid))
+    grid = live_a_generation(grid, sim)
+
+
+print(columns)
 
 
 def main():
