378999a1551080fb6db51dacbfa3aaa970353257
Nickson Kaigi <nickaigi@gmail.com>
2019-04-15 22:56:25 +0300

    ProcessPooleExecutor is blazing fast

--- item_41_concurrent_futures.py
+++ item_41_concurrent_futures.py
@@ -1,4 +1,11 @@
 from time import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+numbers = [
+    (1963309, 2265973), (2030677, 3814172), (1551645, 2229620),
+    (2039045, 2020802)
+]
 
 
 def gcd(pair):
@@ -10,14 +17,46 @@ def gcd(pair):
 
 
 def example_one():
-    numbers = [
-        (1963309, 2265973), (2030677, 3814172), (1551645, 2229620),
-        (2039045, 2020802)
-    ]
+    """
+    >>> 
+    Took 0.282 seconds
+    """
+    start = time()
+    results = list(map(gcd, numbers))
+    end = time()
+    print('Took %.3f seconds' % (end - start))
+
+
+def example_two():
+    """
+    not particularly faster than example_one
+    - overhead of starting and communicating with the pool of threads
+    >>> 
+    Took 0.279 seconds
+    """
+    start = time()
+    # max_workers: match the number of cpu cores on your computer
+    pool = ThreadPoolExecutor(max_workers=4)
+    results = list(pool.map(gcd, numbers))
+    end = time()
+    print('Took %.3f seconds' % (end - start))
+
+
+def example_three():
+    """
+    change to ProcessPoolExecutor
+    >>> 
+    Took 0.100 seconds
+    """
+    start = time()
+    pool = ProcessPoolExecutor(max_workers=4)  # only change
+    results = list(pool.map(gcd, numbers))
+    end = time()
+    print('Took %.3f seconds' % (end - start))
 
 
 def main():
-    example_one()
+    example_three()
 
 
 if __name__ == '__main__':
