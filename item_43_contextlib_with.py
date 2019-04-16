f9e2a1973f98b39628ccb42251266fbe58597237
Nickson Kaigi <nickaigi@gmail.com>
2019-04-16 17:13:05 +0300

    added another fx

--- item_43_contextlib_with.py
+++ item_43_contextlib_with.py
@@ -1,5 +1,7 @@
 import logging
 from threading import Lock
+from contextlib import contextmanager
+logging.getLogger(__name__).setLevel(logging.WARNING)
 
 
 def example_one():
@@ -21,8 +23,46 @@ def example_two():
         lock.release()
 
 
+def my_function():
+    """
+    >>> 
+    ERROR:root:Error here
+    """
+    logging.debug('debug data')
+    logging.error('Error here')
+    logging.debug('more debug')
+
+
+@contextmanager
+def debug_logging(level):
+    logger = logging.getLogger()
+    old_level = logger.getEffectiveLevel()
+    logger.setLevel(level)
+    try:
+       yield 
+    finally:
+        logger.setLevel(old_level)
+
+
+def example_three():
+    """
+    >>> 
+    inside:
+    DEBUG:root:debug data
+    ERROR:root:Error here
+    DEBUG:root:more debug
+    after :
+    ERROR:root:Error here
+    """
+    with debug_logging(logging.DEBUG):
+        print('inside: ')
+        my_function()
+    print('after :')
+    my_function()
+
+
 def main():
-    example_two()
+    example_three()
 
 
 if __name__ == '__main__':
