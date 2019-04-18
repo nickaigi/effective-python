8d022241c3437c8183054b2013c433517c7161f4
Nickson Kaigi <nickaigi@gmail.com>
2019-04-18 22:24:28 +0300

    working to wrap this up

--- item_44_pickle_copyreg.py
+++ item_44_pickle_copyreg.py
@@ -75,8 +75,51 @@ def example_three():
         print('AssertionError: We knew')
 
 
+class GameStateDefaults(object):
+    def __init__(self, level=0, lives=4, points=0):
+        self.level = level
+        self.lives = lives
+        self.points = points
+
+
+def pickle_game_state(game_state):
+    kwargs = game_state.__dict__
+    return unpickle_game_state, (kwargs, )
+
+
+def unpickle_game_state(kwargs):
+    return GameStateDefaults(**kwargs)
+
+
+class GameStateDefaultsMagic(object):
+    def __init__(self, level=0, lives=4, points=0, magic=5):
+        self.level = level
+        self.lives = lives
+        self.points = points
+        self.magic = magic
+
+
+def example_four():
+    """
+    >>> 
+    {'level': 0, 'lives': 4, 'points': 1000}
+    """
+    state = GameStateDefaults()
+    state.points += 1000
+    serialized = pickle.dumps(state)
+    state_after = pickle.loads(serialized)
+    print(state_after.__dict__)
+
+
+def example_five():
+    try:
+        pickle.loads()
+    except:
+        pass
+
+
 def main():
-    example_three()
+    example_four()
 
 
 if __name__ == '__main__':
