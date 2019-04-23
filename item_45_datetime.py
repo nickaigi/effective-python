03b2470ec3e0bc90036eb9ee7d7896f4a435a075
Nickson Kaigi <nickaigi@gmail.com>
2019-04-23 13:45:37 +0300

    conforms to expectation of this codebase

--- item_45_datetime.py
+++ item_45_datetime.py
@@ -1,7 +1,21 @@
 from time import localtime, strftime 
-now = 1407694710
 
-local_tuple = localtime(now)
-time_format = '%Y-%m-%d %H:%M:%S'
-time_str = strftime(time_format, local_tuple)
-print(time_str)
+def example_one():
+    """
+    >>> 
+    2014-08-10 21:18:30
+    """
+    now = 1407694710
+
+    local_tuple = localtime(now)
+    time_format = '%Y-%m-%d %H:%M:%S'
+    time_str = strftime(time_format, local_tuple)
+    print(time_str)
+
+
+def main():
+    example_one()
+
+
+if __name__ == '__main__':
+    main()
