dd5edb7fccc9991c0189a8a950223030879e6f1f
Nickson Kaigi <nickaigi@gmail.com>
2019-04-24 11:01:29 +0300

    wont fix

--- item_45_datetime.py
+++ item_45_datetime.py
@@ -21,8 +21,20 @@ def example_one():
     print(utc_now)
 
 
+def example_two():
+    """
+    >>> 
+    ValueError: time data '2014-05-01 15:45:16 PDT' does not match format '%Y-%m-%d %H:%M:%S %Z'
+    """
+    parse_format = '%Y-%m-%d %H:%M:%S %Z'
+    depart_sfo = '2014-05-01 15:45:16 PDT'
+    time_tuple = strptime(depart_sfo, parse_format)
+    time_str = strftime(time_format, time_tuple)
+    print(time_str) 
+
+
 def main():
-    example_one()
+    example_two()
 
 
 if __name__ == '__main__':
