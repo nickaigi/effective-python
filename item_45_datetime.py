fa712292af3a45cd86288296edc58f6aac46d86f
Nickson Kaigi <nickaigi@gmail.com>
2019-04-24 15:51:54 +0300

    added pytz

--- item_45_datetime.py
+++ item_45_datetime.py
@@ -1,5 +1,7 @@
 from time import localtime, strftime, mktime, strptime
 from datetime import datetime, timezone
+import pytz
+
 
 def example_one():
     """
@@ -30,7 +32,7 @@ def example_two():
     parse_format = '%Y-%m-%d %H:%M:%S %Z'
     depart_sfo = '2014-05-01 15:45:16 PDT'
     time_tuple = strptime(depart_sfo, parse_format)
-    time_str = strftime(time_format, time_tuple)
+    time_str = strftime(parse_format, time_tuple)
     print(time_str) 
 
 
@@ -45,8 +47,35 @@ def example_three():
     print(now_local)
 
 
+def example_four():
+    """
+    >>> 
+    1407658710.0
+    """
+    time_format = '%Y-%m-%d %H:%M:%S'
+    time_str = '2014-08-10 11:18:30'
+    now = datetime.strptime(time_str, time_format)
+    time_tuple = now.timetuple()
+    utc_now = mktime(time_tuple)
+    print(utc_now)
+
+
+def example_five():
+    """
+    >>> 
+    2014-05-02 03:33:24+00:00
+    """
+    time_format = '%Y-%m-%d %H:%M:%S'
+    arrival_nyc = '2014-05-01 23:33:24'
+    nyc_dt_naive = datetime.strptime(arrival_nyc, time_format)
+    eastern = pytz.timezone('US/Eastern')
+    nyc_dt = eastern.localize(nyc_dt_naive)
+    utc_dt = pytz.utc.normalize(nyc_dt.astimezone(pytz.utc))
+    print(utc_dt)
+
+
 def main():
-    example_three()
+    example_two()
 
 
 if __name__ == '__main__':
