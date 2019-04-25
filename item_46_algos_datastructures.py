ab0076f721d2265dcd632d814d6977cd6090dd7f
Nickson Kaigi <nickaigi@gmail.com>
2019-04-25 16:01:24 +0300

    defaultdict

--- item_46_algos_datastructures.py
+++ item_46_algos_datastructures.py
@@ -1,5 +1,6 @@
-from collections import deque, OrderedDict
+from collections import deque, OrderedDict, defaultdict
 from random import randint
+
 
 def example_one():
     """
@@ -31,8 +32,7 @@ def example_two():
     # Randomly populate 'b' to cause hash conflicts
     # Infinite Loop
     while True:
-        #z = randint(99, 1013)
-        z = randint(1, 20)
+        z = randint(99, 1013)
         b = {}
         for i in range(z):
             b[i] = i
@@ -47,8 +47,49 @@ def example_two():
     print('Equal?', a == b)
 
 
+def example_three():
+    """
+    >>> 
+    1 red
+    2 blue
+    """
+    a = OrderedDict()
+    a['foo'] = 1
+    a['bar'] = 2
+
+    b = OrderedDict()
+    b['foo'] = 'red'
+    b['bar'] = 'blue'
+
+    for value1, value2 in zip(a.values(), b.values()):
+        print(value1, value2)
+
+
+def example_five():
+    """
+    dict
+        - you have to check for the existance of a key before accessing the value
+    """
+    stats = {}
+    key = 'my_customer'
+    if key not in stats:
+        stats[key] = 0
+    stats[key] += 1
+
+
+def example_six():
+    """
+    defaultdict
+        - automatically stores a default value when a key doesn't exist.
+        - you have to provide a function that will return the default value
+          each time a key is missing.
+    """
+    stats = defaultdict(int)
+    stats['my_counter'] += 1
+
+
 def main():
-    example_two()
+    example_three()
 
 
 if __name__ == '__main__':
