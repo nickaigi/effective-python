35889072ef4f614cc817db36b72f1bc79db1a16f
Nickson Kaigi <nickaigi@gmail.com>
2019-04-26 17:30:13 +0300

    final edit

--- item_51_root_exceptions.py
+++ item_51_root_exceptions.py
@@ -1,4 +1,8 @@
-"""For APIs it's much more powerful to define your own hierarchy of exceptions"""
+"""For APIs it's much more powerful to define your own hierarchy of exceptions
+
+1. root exceptions let callers understand when there's a problem with their
+   usage of your API
+"""
 
 
 def determine_weight(volume, density):
@@ -15,8 +19,42 @@ class InvalidDensityError(Erro):
     """There was a problem with a provided density value."""
 
 
-def main():
+def example_one():
     try:
         weight = determine_weight(1, -1)
     except Error as e:
         print('Unexpected error: %s', e)
+
+
+def example_two():
+    try:
+        weight = determine_weight(1, -1)
+    except InvalidDensityError:
+        weight = 0
+    except Error as e:
+        print('Bug in the calling code: %s', e)
+
+
+def example_three():
+    try:
+        weight = determine_weight(1, -1)
+    except InvalidDensityError:
+        weight = 0
+    except Error as e:
+        print('Bug in the calling code: %s', e)
+    except Exception as e:
+        print('Bug in the API code: %s', e)
+        raise
+
+
+class NegativeDensityError(InvalidDensityError):
+    """When provided density value is negative"""
+
+
+def new_determine_weight(volume, density):
+    if density < 0:
+        raise NegativeDensityError
+
+
+def main():
+    example_three()
